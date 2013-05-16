3d044ecbe2ed270f083fd235ef003e19c3bf0239
vale <valentina@MacBook-di-Valentina.local>
2013-05-16 17:25:24 +0200

    sdasda

---
+++ PrototipoClassico/PrototipoClassico/OpereViewController.h
@@ -0,0 +1,14 @@
+//
+//  OpereViewController.h
+//  PrototipoClassico
+//
+//  Created by Valentina on 11/05/13.
+//  Copyright (c) 2013 Valentina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpereViewController : UITableViewController
+
+@property (nonatomic,retain) NSMutableArray *data;
+@end
